5599c6fdacd7757b46704de7e1bb1753e3de68ed
Victor Nunes <victornunes470@gmail.com>
2019-09-22 04:00:18 -0300

    receiving search term and search prefix

---
+++ classes/UserInput.js
@@ -0,0 +1,8 @@
+class UserInput {
+    constructor(searchTerm, prefix) {
+        this.searchTerm = searchTerm;
+        this.prefix = prefix;
+    }  
+}
+
+module.exports = UserInput;
---
+++ index.js
@@ -0,0 +1,10 @@
+const readline = require('readline-sync');
+const UserInput = require('./classes/UserInput');
+
+const prefixes = ['Who is', 'What is', 'The History of'];
+
+const searchTerm = readline.question('Type a Wikipedia search term: ');
+
+const prefix = readline.keyInSelect(prefixes, 'Choose a option');
+
+const input = new UserInput(searchTerm, prefix);
