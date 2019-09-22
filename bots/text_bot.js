43f56e94dbe233f3f27a86c5034a99df479d9451
Victor Nunes <victornunes470@gmail.com>
2019-09-22 11:12:30 -0300

    wip text bot methods

--- bots/text_bot.js
+++ bots/text_bot.js
@@ -2,6 +2,18 @@ class TextBot {
     constructor(completeSearchTerm) {
         console.log(`Receive ${completeSearchTerm.searchTerm}`);
     }
+
+    getFromWikipedia(content) {
+
+    }
+
+    clean(content) {
+
+    }
+
+    breakIntoSentences(content) {
+
+    }
 }
 
 module.exports = TextBot;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
 const UserInput = require('./classes/UserInput');
+const TextBot = require('./bots/text_bot');
 
 const prefixes = ['Who is', 'What is', 'The History of'];
 
@@ -10,3 +11,9 @@ const prefix = readline.keyInSelect(prefixes, 'Choose a option');
 console.log(prefix);
 
 const input = new UserInput(searchTerm, prefix);
+const cst = input.getCompleteSearchTerm();
+
+const tb = new TextBot(cst);
+
+
+
